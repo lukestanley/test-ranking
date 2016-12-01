db01f026e7c4a8a86a86a8914eea6b9d1dfec827
Luke Stanley <luke.stanley@gmail.com>
2016-12-01 13:24:37 +0000

    Implemented view and wishlist sort

--- ranker/data/item_ranker.py
+++ ranker/data/item_ranker.py
@@ -4,6 +4,18 @@ __author__ = 'shillaker'
 
 
 def rank_all_items():
-    items = Item.objects.order_by('name')
+    items = list(Item.objects.all())
+    def view_and_wish_sort(i):
+        total_item_views = i.itemview_set.count()
+        total_wish_list_entries = i.wishlistentry_set.count()
+        item_popularity_score = total_item_views + (2 * total_wish_list_entries)
+        return item_popularity_score
+    items = sorted(items, key=view_and_wish_sort)
+    
+    return items
 
-    return list(items)
+"""
+items = Item.objects.all()
+
+for item in items:
+    print(item)"""
